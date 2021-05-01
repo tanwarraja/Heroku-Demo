2d0e7258978da0ac7627b760b2aac447fdecc8f1
tanwarraja <57171458+tanwarraja@users.noreply.github.com>
2021-05-02 00:13:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     app.run(debug=True)
 
 
